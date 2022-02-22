d052d672399aa1e44c4e1e0d150458b5d91e2bf8
Joseph Rousoss <jroussos01@gmail.com>
2022-02-22 17:57:48 -0500

    Added some scuffed state cameras

---
+++ Not_Real_GameJam_Submission/Assets/Scripts/Cameras/StateCameras.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StateCameras : MonoBehaviour
+{
+    Animator myAnim;
+    private int CurrLevel = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    private void Awake()
+    {
+        CurrLevel = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(CurrLevel)
+        {
+            case 0:
+                Level0();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void Level0()
+    {
+        Vector3 Playerpos = gameObject.transform.position;
+        if (Playerpos.x > 63)
+        {
+            myAnim.SetBool("Player bottom", true);
+            myAnim.SetBool("Player top", false);
+        }
+        else
+        {
+            myAnim.SetBool("Player top", true);
+            myAnim.SetBool("Player bottom", false);
+        }
+    }
+}
