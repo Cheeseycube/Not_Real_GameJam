8431754f6130622f1da05122068b27172a51eb59
Joseph Rousoss <jroussos01@gmail.com>
2022-02-21 20:24:35 -0500

    basic player completed

--- Not_Real_GameJam_Submission/Assets/Scripts/Player.cs
+++ Not_Real_GameJam_Submission/Assets/Scripts/Player.cs
@@ -4,15 +4,85 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Components
+    Rigidbody2D rb;
+    PolygonCollider2D BodyCollider;
+    BoxCollider2D FeetCollider;
+
+    // Serialized Fields
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 11f;
+    [SerializeField] float heldjumpSpeed = 5f;
+
+    // floats
+    float mayJump = 0.1f;
+    float timer;
+
+    // integers
+
+    // booleans
+    private bool isDead = false;
+    private bool isJumping = false;
     
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        BodyCollider = GetComponent<PolygonCollider2D>();
+        FeetCollider = GetComponent<BoxCollider2D>();
     }
 
     
     void Update()
     {
-        
+        Run();
+        Jump();
+    }
+
+    private void Run()
+    {
+        if (isDead)
+        {
+            return;
+        }
+        float horizontalInput = Input.GetAxisRaw("Horizontal"); // value between -1 and +1
+        rb.velocity = new Vector2(horizontalInput * runSpeed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        if (isDead)
+        {
+            return;
+        }
+
+        mayJump -= Time.deltaTime;
+
+        if (FeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            mayJump = 0.1f;
+        }
+
+        if (Input.GetButtonDown("Jump") && (mayJump > 0f))
+        {
+            print("jumping");
+            timer = Time.time;
+            isJumping = true;
+            rb.velocity = new Vector2(0f, jumpSpeed);
+        }
+
+        if (Input.GetButton("Jump") && isJumping)
+        {
+            if (Time.time - timer > 0.15) // make 0.15 bigger for longer time needed to hold down jump
+            {
+                timer = float.PositiveInfinity;
+                rb.velocity += new Vector2(0f, heldjumpSpeed);
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+        }
     }
 }
