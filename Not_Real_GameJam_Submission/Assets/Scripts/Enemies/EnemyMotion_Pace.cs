6596dfb6735bb75bf254c3f94789241317f22516
ntm-02 <noahmitchell007@gmail.com>
2022-02-22 22:09:36 -0500

    wwwwwwwwwwwwwwwwhat

--- Not_Real_GameJam_Submission/Assets/Scripts/Enemies/EnemyMotion_Pace.cs
+++ Not_Real_GameJam_Submission/Assets/Scripts/Enemies/EnemyMotion_Pace.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class EnemyMotion_Pace : MonoBehaviour
 {
+    Rigidbody2D body;
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = this.GetComponentInParent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        body.velocity = new Vector2(-6f, 0);
     }
 }
