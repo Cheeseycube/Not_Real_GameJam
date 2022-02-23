d118113e9dbf4727b7456a65b2995789c3b211a0
Joseph Rousoss <jroussos01@gmail.com>
2022-02-23 16:16:32 -0500

    setting up opening scene

---
+++ Not_Real_GameJam_Submission/Assets/Scripts/Player Scripts/CutscenePlayer.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutscenePlayer : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    private float timer;
+    private bool isWalking = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        timer = 1f;
+        StartCoroutine(Walking());
+    }
+
+    private void Awake()
+    {
+        //timer = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - timer < 3.2f)
+        {
+            StartCoroutine(Walking());
+            print("This is the current time:" + Time.time);
+            print("This is the timer:" + timer);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0f, 0f);
+        }
+    }
+
+    IEnumerator Walking()
+    {
+        yield return new WaitForSeconds(2f);
+        if (!isWalking)
+        {
+            rb.velocity = new Vector2(3.5f, 0f);
+            isWalking = true;
+        }
+    }
+}
