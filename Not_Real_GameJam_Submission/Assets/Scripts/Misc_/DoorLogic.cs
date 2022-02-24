dbd843423f4978407ca7887ef646f4a01c4e461d
Joseph Rousoss <jroussos01@gmail.com>
2022-02-23 22:56:52 -0500

    basic door logic

---
+++ Not_Real_GameJam_Submission/Assets/Scripts/Misc_/DoorLogic.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLogic : MonoBehaviour
+{
+    Animator myAnim;
+    SpriteRenderer myRenderer;
+
+    public Sprite doorOpen;
+    public Sprite doorClosed;
+
+    private bool door_open = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+        myRenderer = GetComponent<SpriteRenderer>();
+        myRenderer.sprite = doorOpen;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            DoorInteract();
+        }
+    }
+
+    public void DoorInteract()
+    {
+        if (!door_open)
+        {
+            myAnim.SetBool("Door opening", true);
+            StartCoroutine(stopOpening());
+            door_open = true;
+            //myRenderer.sprite = doorOpen;
+        }
+        else
+        {
+            myAnim.SetBool("Door closing", true);
+            StartCoroutine(stopClosing());
+            door_open = false;
+            //myRenderer.sprite = doorClosed;
+        }
+    }
+
+    IEnumerator stopOpening()
+    {
+        yield return new WaitForSeconds(1f);
+        myAnim.SetBool("Door opening", false);
+    }
+
+    IEnumerator stopClosing()
+    {
+        yield return new WaitForSeconds(1f);
+        myAnim.SetBool("Door closing", false);
+    }
+}
+
+
