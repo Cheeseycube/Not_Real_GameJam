419f2d6d46fe583966be0fc03aa1979df5735fd9
Joseph Rousoss <jroussos01@gmail.com>
2022-02-26 21:22:39 -0500

    frogger scene cars wrap around

---
+++ Not_Real_GameJam_Submission/Assets/Scripts/Enemies/CarMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    public GameObject WayPoint;
+    public Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveForward();
+        //transform.position = Vector2.MoveTowards(transform.position, WayPoint.transform.position, Time.deltaTime * speed);
+    }
+
+    private void moveForward()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, WayPoint.transform.position, Time.deltaTime * speed);
+        if (transform.position == WayPoint.transform.position)
+        {
+            transform.position = startPos;
+        }
+    }
+}
