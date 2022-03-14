5a039e38e95abac378598a06af2a7af745103209
Joseph Rousoss <jroussos01@gmail.com>
2022-03-14 00:54:40 -0400

    small changes to boss scene

--- Not_Real_GameJam_Submission/Assets/Scripts/Player Scripts/Fighting_Player.cs
+++ Not_Real_GameJam_Submission/Assets/Scripts/Player Scripts/Fighting_Player.cs
@@ -7,26 +7,39 @@ public class Fighting_Player : MonoBehaviour
     // Components
     Rigidbody2D rb;
     PolygonCollider2D bodyCollider;
+    BoxCollider2D FeetCollider;
     Animator myAnim;
 
     // Booleans
     private bool PlayerDead = false;
     private bool Paused = false;
+    private bool isJumping = false;
 
     // floats and ints
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 11f;
+    [SerializeField] float heldjumpSpeed = 5f;
+    private float mayJump = 0.1f;
+    private float timer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         bodyCollider = GetComponent<PolygonCollider2D>();
+        FeetCollider = GetComponent<BoxCollider2D>();
         myAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // teleports player up if they get pushed under the ground
+        if (transform.position.y < -2.8f)
+        {
+            transform.Translate(0, 0.2f, 0);
+        }
         RunAnimation();
+        Jump();
     }
 
     private void FixedUpdate()
@@ -53,6 +66,43 @@ public class Fighting_Player : MonoBehaviour
         else if (horizontalInput > 0)
         {
             gameObject.transform.localScale = new Vector2(3, gameObject.transform.localScale.y);
+        }
+    }
+
+    private void Jump()
+    {
+        if (PlayerDead || Player.Paused)
+        {
+            return;
+        }
+
+        mayJump -= Time.deltaTime;
+
+        if (FeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            mayJump = 0.1f;
+        }
+
+        if (Input.GetButtonDown("Jump") && (mayJump > 0f))
+        {
+            timer = Time.time;
+            isJumping = true;
+            rb.velocity = new Vector2(0f, jumpSpeed);
+        }
+
+        if (Input.GetButton("Jump") && isJumping)
+        {
+            if (Time.time - timer > 0.15) // make 0.15 bigger for longer time needed to hold down jump
+            {
+                timer = float.PositiveInfinity;
+                rb.velocity += new Vector2(0f, heldjumpSpeed);
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
         }
     }
 
